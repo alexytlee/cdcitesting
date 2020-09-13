f02e792dc4973374575dfbfd4429fb817a705e53
alexytlee <alexytlee@gmail.com>
2020-09-14 01:02:58 +0800

    setting up travis

--- config/dev.js
+++ config/dev.js
@@ -5,4 +5,5 @@ module.exports = {
   mongoURI:
     "mongodb://alexleerious:A7FaNt2ZtmvzNbbp@cluster0-shard-00-00.0ttli.mongodb.net:27017,cluster0-shard-00-01.0ttli.mongodb.net:27017,cluster0-shard-00-02.0ttli.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-fvqyoz-shard-0&authSource=admin&retryWrites=true&w=majority",
   cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
 };
